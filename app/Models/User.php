3be410248511c81f898c5cdb6b129ebf2c73603c
ulanovka12 <valya.nurbaev.89@mail.ru>
2026-09-03 20:57:20 +0800

    середина первичных моделей пройдена

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,23 +2,58 @@
 
 namespace App\Models;
 
- use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Models\Order;
 use Database\Factories\UserFactory;
-use Illuminate\Database\Eloquent\Attributes\Fillable;
-use Illuminate\Database\Eloquent\Attributes\Hidden;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-#[Fillable(['name', 'email', 'password'])]
-#[Hidden(['password', 'remember_token'])]
+/**
+ * @property string $first_name
+ * @property string $last_name
+ * @property string $email
+ * @property string $phone
+ * @property string $password
+ *
+ * @property-read string $full_name
+ *
+ * @property Collection|Order[] $orders
+ * @property Collection|Cart[] $carts
+ */
+
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
+
     use HasFactory, Notifiable;
 
     /**
-     * Get the attributes that should be cast.
+     * Атрибуты, доступные для массового заполнения.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'email',
+        'email_verified_at',
+        'phone',
+        'password'
+    ];
+
+    /**
+     * Атрибуты, скрытые при сериализации.
+     *
+     * @var list<string>
+     */
+    protected $hidden = [
+        'password',
+    ];
+
+    /**
+     * Преобразование атрибутов.
      *
      * @return array<string, string>
      */
@@ -28,5 +63,23 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    /** Связь с заказами */
+    public function orders()
+    {
+        return $this->hasMany(Order::class);
+    }
+
+    /** Связь с корзинами */
+    public function carts()
+    {
+        return $this->hasMany(Cart::class);
+    }
+
+    /** Полное имя */
+    public function getFullNameAttribute(): string
+    {
+        return "{$this->first_name} {$this->last_name}";
     }
 }
